322b5d600599b1acd48f6efa8b234cde20ecc729
Vijay <vijay.chacko56@gmail.com>
2019-04-08 14:09:36 -0500

    Nav menu colapse and admin page items

--- src/App.js
+++ src/App.js
@@ -21,12 +21,20 @@ class App extends Component {
 
   constructor(){
    super();
+   this.toggleNavbar = this.toggleNavbar.bind(this);
    this.state = {
     route:'login',
     user:{
       email:''
     }
    }
+ }
+
+
+ toggleNavbar() {
+ this.setState({
+ collapsed: !this.state.collapsed,
+ });
  }
 
 loadUser = (data) =>{
@@ -53,6 +61,11 @@ this.setState({route:route});
 
 
   render() {
+
+     const collapsed = this.state.collapsed;
+ const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+ const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+
     return (
       <div className="App">
     <head>
@@ -64,13 +77,13 @@ this.setState({route:route});
 <body>
 <nav className="navbar navbar-expand-lg mb-4 top-bar navbar-static-top sps sps--abv">
                 <div className="container">
-                    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse1" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <button onClick={this.toggleNavbar} className={`${classTwo}`}   type="button" data-toggle="collapse" data-target="#navbarCollapse1" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"><i className="fa fa-bars" aria-hidden="true"></i></span>
                     </button>
             
                     <a className="navbar-brand logo" href="#" onClick={()=> this.onRouteChange('home')}><img src ="/images/logo.jpg" alt="Logo" /></a> 
                     <a className="navbar-brand mx-auto" href="#" onClick={()=> this.onRouteChange('home')}>Canis <span data-blast="color">Ninja</span></a>
-                    <div className="collapse navbar-collapse" id="navbarCollapse1">
+                    <div  className={`${classOne}`}  id="navbarCollapse1">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item"> <a className="nav-link" onClick={() => this.onRouteChange('home')} data-blast="color"><strong>Home</strong> <span className="sr-only">(current)</span></a> </li>
                             <li className="nav-item"> <a className="nav-link"  href="#" onClick={() => this.onRouteChange('services')}><strong>Services</strong></a> </li>
