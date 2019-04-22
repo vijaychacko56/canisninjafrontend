60629a9e32ad4e7551f34ccb4b4628ef878fecb1
Vijay <vijay.chacko56@gmail.com>
2019-04-22 00:21:23 -0500

    updating link urls to heroku server url so that it does not go to error page

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -12,7 +12,7 @@ const Error = () =>{
 				</section>
 			<div class="link-container">
 				<h6 style={{'padding-top': '35px'}} >Page Not Found!!I'm just going around in circles here. Please take me Home</h6>
-				<a  class="more-link" href="http://localhost:3000/#">
+				<a  class="more-link" href="https://canisninja.herokuapp.com/">
 
 					<div class="col-md-3 banf" style={{'margin': '2% auto', 'background-color': 'orange','width': '10%','padding': '2%'}}>
                                <button type="button">Home</button>
