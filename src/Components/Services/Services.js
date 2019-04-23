035e5f2549ca17249068f25e28bf1c6e58ac65e4
Vijay <vijay.chacko56@gmail.com>
2019-04-22 23:04:48 -0500

    Changing href lnks in services page

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -147,7 +147,7 @@ else{
                             Our dog handlers can spend some time with your pets and keep them energized. </p>
                             <p><b>Price: $25.00</b></p>
                         <div class="log-in mt-md-4 mt-3">
-                            <a style={{backgroundColor: 'white', padding: '0em 0em'}}href='#book'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
+                            <a  data-target="#book" style={{backgroundColor: 'white', padding: '0em 0em'}}href='https://canisninja.herokuapp.com/#'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
                              Book Now</button></a>
                         </div>
 
@@ -171,7 +171,8 @@ else{
                                 Our dog handlers will assist you in taking your pet to that important appointment. </p>
                                 <p><b>Price: $45.00</b></p>
                         <div class="log-in mt-md-4 mt-3">
-                        <a style={{backgroundColor: 'white', padding: '0em 0em'}}href='#book'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
+                        <div class="log-in mt-md-4 mt-3">
+                            <a  data-target="#book" style={{backgroundColor: 'white', padding: '0em 0em'}}href='https://canisninja.herokuapp.com/#'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
                              Book Now</button></a>
                         </div>
 
@@ -196,7 +197,7 @@ else{
                                 They will be at your doorstep at scheduled time.</p>
                                 <p><b>Price: $15.00</b></p>
                         <div class="log-in mt-md-4 mt-3">
-                        <a style={{backgroundColor: 'white', padding: '0em 0em'}}href='#book'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
+                            <a  data-target="#book" style={{backgroundColor: 'white', padding: '0em 0em'}}href='https://canisninja.herokuapp.com/#'><button className="login100-form-btn" style={buttonStyle} data-blast="bgColor">
                              Book Now</button></a>
                         </div>
 
